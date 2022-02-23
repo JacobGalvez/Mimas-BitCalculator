12082ccc38bb47a0989211a5f1700c2764d47c6b
Jacob Galvez <jag741@txstate.edu>
2022-02-22 18:44:34 -0600

    BIT-13
    Created Hexadecimal to Binary Method

--- app/src/main/java/com/example/mimas_bitcalc/Conversion_Code.kt
+++ app/src/main/java/com/example/mimas_bitcalc/Conversion_Code.kt
@@ -1,4 +1,16 @@
 package com.example.mimas_bitcalc
 
 class Conversion_Code {
+
+    public void hexadecimalToBinary() {
+        String userHexadecimalInput = "0A"; //  later add user input, will be an argument in the function
+
+        int hexadecimalToDecimal = Integer.parseInt(userHexadecimalInput, 16);
+
+        String userInputConversion = (Integer.toBinaryString(hexadecimalToDecimal));
+
+        System.out.println("Conversion (Should be 10) : " + userInputConversion);
+    }
+
+
 }

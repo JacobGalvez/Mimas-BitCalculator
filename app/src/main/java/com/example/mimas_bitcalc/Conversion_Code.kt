2fb9592924a26baa3828abc61553b04221b70ed5
Matthew Whieldon <mjw223@txstate.edu>
2022-02-24 17:31:58 -0600

    BIT-7
    Completed function for Binary to Decimal

--- app/src/main/java/com/example/mimas_bitcalc/Conversion_Code.kt
+++ app/src/main/java/com/example/mimas_bitcalc/Conversion_Code.kt
@@ -1,4 +1,20 @@
 package com.example.mimas_bitcalc
 
 class Conversion_Code {
+
+    fun binToDec(num1: String): String {
+        var binary = num1.toLong()
+        var decimal = 0
+        var i = 0
+        var digit: Long
+
+        while (binary.toInt() != 0) {
+            digit = binary % 10
+            binary /= 10
+            decimal += (digit * Math.pow(2.0, i.toDouble())).toInt()
+            ++i
+        }
+        return decimal.toString()
+    }
+    
 }

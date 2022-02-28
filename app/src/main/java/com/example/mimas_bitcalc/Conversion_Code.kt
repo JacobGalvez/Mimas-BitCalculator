74a5d62e2d489736b6014b54b1c88e61fefa9ee7
Matthew Whieldon <mjw223@txstate.edu>
2022-02-28 13:33:24 -0600

    BIT-15

--- app/src/main/java/com/example/mimas_bitcalc/Conversion_Code.kt
+++ app/src/main/java/com/example/mimas_bitcalc/Conversion_Code.kt
@@ -16,5 +16,68 @@ class Conversion_Code {
         }
         return decimal.toString()
     }
+
+    fun binToHex(num1: String): String {
+        var binary =  num1.toLong()
+        var hex = ""
+        var Digits: Long
+        var fourDigits: Int
+
+        while(binary.toInt() != 0){
+            Digits = binary % 10000
+            fourDigits = Digits.toInt()
+            if(fourDigits == 0){
+                hex += "0"
+            }
+            else if(fourDigits == 1){
+                hex += "1"
+            }
+            else if(fourDigits == 10){
+                hex += "2"
+            }
+            else if(fourDigits == 11){
+                hex += "3"
+            }
+            else if(fourDigits == 100){
+                hex += "4"
+            }
+            else if(fourDigits == 101){
+                hex += "5"
+            }
+            else if(fourDigits == 110){
+                hex += "6"
+            }
+            else if(fourDigits == 111){
+                hex += "7"
+            }
+            else if(fourDigits == 1000){
+                hex += "8"
+            }
+            else if(fourDigits == 1001){
+                hex += "9"
+            }
+            else if(fourDigits == 1010){
+                hex += "A"
+            }
+            else if(fourDigits == 1011){
+                hex += "B"
+            }
+            else if(fourDigits == 1100){
+                hex += "C"
+            }
+            else if(fourDigits == 1101){
+                hex += "D"
+            }
+            else if(fourDigits == 1110){
+                hex += "E"
+            }
+            else if(fourDigits == 1111){
+                hex += "F"
+            }
+            binary /= 10000
+        }
+        hex = hex.reversed()
+        return hex
+    }
     
 }

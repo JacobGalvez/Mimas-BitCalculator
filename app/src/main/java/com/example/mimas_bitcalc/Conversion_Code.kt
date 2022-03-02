dd988bc92e2d5c8477950578c97c3d862e9427fb
Jacob Galvez <jag741@txstate.edu>
2022-03-02 17:43:37 -0600

    Updated Conversion Functions to take args.

--- app/src/main/java/com/example/mimas_bitcalc/Conversion_Code.kt
+++ app/src/main/java/com/example/mimas_bitcalc/Conversion_Code.kt
@@ -2,14 +2,22 @@ package com.example.mimas_bitcalc
 
 class Conversion_Code {
 
-    public void hexadecimalToBinary() {
-        String userHexadecimalInput = "0A"; //  later add user input, will be an argument in the function
+    public void hexadecimalToBinary(String userHexadecimalInput) {
+        //String userHexadecimalInput = "0A"; //  later add user input, will be an argument in the function
 
         int hexadecimalToDecimal = Integer.parseInt(userHexadecimalInput, 16);
 
         String userInputConversion = (Integer.toBinaryString(hexadecimalToDecimal));
 
         System.out.println("Conversion (Should be 10) : " + userInputConversion);
+    }
+
+    public void hexadecimalToDecimal(String userHexNumber) {
+        //String userHexNumber = "0A";
+
+        int userNumberConversion = Integer.parseInt(userHexNumber, 16);
+
+        System.out.println("Hex: " + userHexNumber + " Converted to Decimal: " + userNumberConversion);
     }
 
 

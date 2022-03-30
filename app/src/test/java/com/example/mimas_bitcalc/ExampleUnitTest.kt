8f63bf946b1a58423c9574eb0c81d223f29ae64f
Matthew Whieldon <mjw223@txstate.edu>
2022-03-30 11:25:43 -0500

    BIT-57, BIT-40
    Gradle outputs to console and 2 tests for decimal to binary conversion.

--- app/src/test/java/com/example/mimas_bitcalc/ExampleUnitTest.kt
+++ app/src/test/java/com/example/mimas_bitcalc/ExampleUnitTest.kt
@@ -1,17 +1,32 @@
 package com.example.mimas_bitcalc
 
-import org.junit.Test
+//import org.junit.Test
+//import org.junit.Assert.*
 
-import org.junit.Assert.*
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-class ExampleUnitTest {
+class JUnitTesting {
+    internal val converter = Conversion_Code()
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    @DisplayName("Decimal to Binary: Equals")
+    fun decToBinEquals() {
+        var ans = converter.decToBin("15")
+        assertEquals(ans, "1111")
     }
+    @Test
+    @DisplayName("Decimal to Binary: True")
+    fun decToBinTrue() {
+        var ans = converter.decToBin("10")
+        assertTrue(ans == "1010")
+    }
+    
 }

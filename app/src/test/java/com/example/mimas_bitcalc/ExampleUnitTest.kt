da092f756fcd73fc5f4e0604c47192725935c32b
Matthew Whieldon <mjw223@txstate.edu>
2022-03-31 14:18:34 -0500

    BIT-40 added more decimal tests

--- app/src/test/java/com/example/mimas_bitcalc/ExampleUnitTest.kt
+++ app/src/test/java/com/example/mimas_bitcalc/ExampleUnitTest.kt
@@ -15,7 +15,17 @@ import org.junit.jupiter.api.Test
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class JUnitTesting {
+
     internal val converter = Conversion_Code()
+
+    //dec to binary tests
+    @Test
+    @DisplayName("Decimal to Binary to Decimal")
+    fun decToBinToDec() {
+        var ans = converter.decToBin("10")
+        ans = converter.binToDec(ans)
+        assertEquals(ans, "10")
+    }
     @Test
     @DisplayName("Decimal to Binary: Equals")
     fun decToBinEquals() {
@@ -28,5 +38,39 @@ class JUnitTesting {
         var ans = converter.decToBin("10")
         assertTrue(ans == "1010")
     }
+    @Test
+    @DisplayName("Decimal to Binary: False")
+    fun decToBinFalse() {
+        var ans = converter.decToBin("10")
+        assertFalse(ans == "10")
+    }
+
+    //dec to hex tests
+    @Test
+    @DisplayName("Decimal to Hex to Decimal")
+    fun decToHexToDec() {
+        var ans = converter.decToHex("10")
+        ans = converter.hexadecimalToDecimal(ans)
+        assertEquals(ans, "10")
+    }
+    @Test
+    @DisplayName("Decimal to Hex: True")
+    fun decToHexTrue() {
+        var ans = converter.decToHex("123")
+        assertTrue(ans == "7B")
+    }
+    @Test
+    @DisplayName("Decimal to Hex: Equals")
+    fun decToHexEquals() {
+        var ans = converter.decToHex("321")
+        assertEquals(ans, "141")
+    }
+    @Test
+    @DisplayName("Decimal to Hex: False")
+    fun decToHexFalse() {
+        var ans = converter.decToHex("22")
+        assertFalse(ans == "141")
+    }
+    
     
 }

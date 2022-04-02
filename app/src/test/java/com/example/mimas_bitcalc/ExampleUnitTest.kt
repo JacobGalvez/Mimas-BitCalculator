83a625a9503fe8977716dd4ad2dac7a768a78e29
Matthew Whieldon <mjw223@txstate.edu>
2022-04-02 14:58:48 -0500

    BIT-42
    Added testing for hex conversions

--- app/src/test/java/com/example/mimas_bitcalc/ExampleUnitTest.kt
+++ app/src/test/java/com/example/mimas_bitcalc/ExampleUnitTest.kt
@@ -46,25 +46,25 @@ class JUnitTesting {
     }
 
     //dec to hex tests
-    /*@Test
+    @Test
     @DisplayName("Decimal to Hex to Decimal")
     fun decToHexToDec() {
         var ans = converter.decToHex("10")
         ans = converter.hexadecimalToDecimal(ans)
         assertEquals(ans, "10")
-    }*/
-    /*@Test
+    }
+    @Test
     @DisplayName("Decimal to Hex: True")
     fun decToHexTrue() {
         var ans = converter.decToHex("123")
-        assertTrue(ans == "7B")
-    }*/
-    /*@Test
+        assertTrue(ans == "7b")
+    }
+    @Test
     @DisplayName("Decimal to Hex: Equals")
     fun decToHexEquals() {
         var ans = converter.decToHex("321")
         assertEquals(ans, "141")
-    }*/
+    }
     @Test
     @DisplayName("Decimal to Hex: False")
     fun decToHexFalse() {
@@ -72,5 +72,58 @@ class JUnitTesting {
         assertFalse(ans == "141")
     }
     
+    //hex to bin tests
+    @Test
+    @DisplayName("Hex to Binary to Hex")
+    fun hexToBinToHex() {
+        var ans = converter.hexadecimalToBinary("F")
+        ans = converter.binToHex(ans)
+        assertEquals(ans, "f")
+    }
+    @Test
+    @DisplayName("Hex to Binary: True")
+    fun hexToBinTrue() {
+        var ans = converter.hexadecimalToBinary("FF")
+        assertTrue(ans == "11111111")
+    }
+    @Test
+    @DisplayName("Hex to Binary: Equals")
+    fun hexToBinEquals() {
+        var ans = converter.hexadecimalToBinary("9")
+        assertEquals(ans, "1001")
+    }
+    @Test
+    @DisplayName("Hex to Binary: False")
+    fun hexToBinFalse() {
+        var ans = converter.hexadecimalToBinary("B")
+        assertFalse(ans == "1010")
+    }
+
+    //hex to dec tests
+    @Test
+    @DisplayName("Hex to Decimal to Hex")
+    fun hexToDecToHex() {
+        var ans = converter.decToHex("10")
+        ans = converter.hexadecimalToDecimal(ans)
+        assertEquals(ans, "10")
+    }
+    @Test
+    @DisplayName("Hex to Decimal: True")
+    fun hexToDecTrue() {
+        var ans = converter.decToHex("123")
+        assertTrue(ans == "7b")
+    }
+    @Test
+    @DisplayName("Hex to Decimal: Equals")
+    fun hexToDecEquals() {
+        var ans = converter.decToHex("321")
+        assertEquals(ans, "141")
+    }
+    @Test
+    @DisplayName("Hex to Decimal: False")
+    fun hexToDecFalse() {
+        var ans = converter.decToHex("22")
+        assertFalse(ans == "141")
+    }
     
 }
